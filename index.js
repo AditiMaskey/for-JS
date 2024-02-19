59909fea21c08b20d8a828526e807bdc6cee351a
AdiM <adimaskey0@gmail.com>
2024-02-19 10:59:41 -0500

    Added the files

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log("This is the first line");
+
+// document.write("This is what I know about JS.");
+
+//Variable
+
+//var: Don't use it
+//let:Most 2nd 
+//const: Most used
+
+let firstName = "Dikshya";
+let age = 23;
+const job = "Employed"; 
+
+console.log(job, age);
+
+console.log("My status is " + job);
+
+console.log("I am", age);
+
+
+console.log(`My name is ${firstName}. I am ${age} years old and I am ${job}!!`);
+
+// job = "unemployed";
+
+age = 30;
+
+console.log("The job is:", job);
+
+console.log("Age:", age);
+
+
+// console.log(undeclared);
+
+// var undeclared = "Something";
+
+// let undeclared = "Something";
+
